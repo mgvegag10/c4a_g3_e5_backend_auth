80c836fa84c22475fc48b015e87cf830cf6dcdb6
mgvegag10 <mgvegag10@gmail.com>
2022-09-14 18:46:42 -0500

    configuracion inicial de la aplicacion en spring boot

---
+++ src/main/java/c4a_g3_e5_backend/seguridad/SeguridadApplication.java
@@ -0,0 +1,13 @@
+package c4a_g3_e5_backend.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c4a_g3_e5_backend/seguridad/SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package c4a_g3_e5_backend.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
